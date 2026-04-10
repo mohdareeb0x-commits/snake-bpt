d24358b8e82d88e3247845f9039ea92b0032c360
Mohd Areeb <mohdareeb0x@gmail.com>
2026-04-10 19:15:27 +0530

    Handled fastapi command logic

--- cmd/fastapi.go
+++ cmd/fastapi.go
@@ -1,12 +1,13 @@
 /*
 Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
-*/ 
+*/
 package cmd
 
 import (
-	"fmt"
+	"os"
+	"snake-bpt/logic"
 
+	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 )
 
@@ -21,7 +22,34 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("fastapi called")
+
+		host, err := cmd.Flags().GetString("host")
+		if err != nil {
+			pterm.Error.Println(err)
+			os.Exit(1)
+		}
+		
+		if !logic.ValidateIP(host) {
+			os.Exit(1)
+		}
+
+		port, err := cmd.Flags().GetString("port")
+		if err != nil {
+			pterm.Error.Println(err)
+			os.Exit(1)
+		}
+
+		if !logic.ValidatePort(port) {
+			os.Exit(1)
+		}
+
+		err = logic.AddFastAPI(host, port)
+		if err != nil {
+			pterm.Error.Println(err)
+			os.Exit(1)
+		}
+
+		pterm.Success.Println("FastAPI project initialized")
 	},
 }
 
@@ -37,4 +65,7 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// fastapiCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	fastapiCmd.Flags().String("host", "0.0.0.0", "IP address to bind")
+	fastapiCmd.Flags().String("port", "8000", "Port to bind")
 }
